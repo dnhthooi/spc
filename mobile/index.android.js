3a0aed5db5bcec699db230ce3f6b3dafa1d0be6e
Tam Dao <daotam4536@gmail.com>
2017-03-20 17:02:54 +0700

    Update: index.android.js

--- mobile/index.android.js
+++ mobile/index.android.js
@@ -4,50 +4,39 @@
  * @flow
  */
 
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
+
+import { Provider } from 'react-redux';
+
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
 
+import thunk from "redux-thunk";
+
+import App from './app/App';
+import reducer from './app/reducers';
+
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk)
+);
+
 export default class mobile extends Component {
+
+  constructor() {
+    super();
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return (<Provider store={store}>
+      <App />
+    </Provider>);
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('mobile', () => mobile);
